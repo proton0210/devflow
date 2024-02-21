5723848cb942ad49f04830f6b0308bfafa67664e
Vidit Shah <vidit0210@protonmail.com>
2024-02-21 15:15:54 +0530

    assets folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description: "DevFlow is a platform for developers to learn and grow",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
