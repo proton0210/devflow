78657c7f2e423efa31be16dc5a8217b9fd43cce3
Vidit Shah <vidit0210@protonmail.com>
2024-02-22 17:31:12 +0530

    Left Side Bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
---
+++ components/shared/search/GlobalSearch.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchContainerRef = useRef(null);
+
+  const query = searchParams.get("q");
+
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className="relative w-full max-w-[600px] max-lg:hidden"
+      ref={searchContainerRef}
+    >
+      <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={18}
+          height={18}
+          className="cursor-pointer"
+        />
+
+        <Input
+          type="text"
+          placeholder="Search globally"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "" && isOpen) setIsOpen(false);
+          }}
+          className="paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GlobalSearch;
