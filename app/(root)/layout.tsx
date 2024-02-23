45b9b5d2932d6ec89125b23d44965d5971fb42b4
Vidit Shah <vidit0210@protonmail.com>
2024-02-23 19:28:55 +0530

    added Local Search Bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSideBar from "@/components/shared/RightSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -15,7 +16,7 @@ export default function RootLayout({
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSideBar
+        <RightSideBar />
       </div>
       Toaster
     </main>
