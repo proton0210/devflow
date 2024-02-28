76588a4db4826ba98519dedc40ac14f21ea2658c
Vidit Shah <vidit0210@protonmail.com>
2024-02-28 16:18:33 +0530

    Answer form

--- lib/actions/question.actions.ts
+++ lib/actions/question.actions.ts
@@ -1,12 +1,15 @@
 'use server'
 
-import Tag from '@/database/tag.model'
-import { revalidatePath } from 'next/cache'
-import { connectToDatabase } from '../mongoose'
-
 import Question from '@/database/question.model'
-import { CreateQuestionParams, GetQuestionsParams } from './shared.types'
+import Tag from '@/database/tag.model'
+import { connectToDatabase } from '../mongoose'
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams
+} from './shared.types'
 import User from '@/database/user.model'
+import { revalidatePath } from 'next/cache'
 
 export async function getQuestions (params: GetQuestionsParams) {
   try {
@@ -23,6 +26,7 @@ export async function getQuestions (params: GetQuestionsParams) {
     throw error
   }
 }
+
 export async function createQuestion (params: CreateQuestionParams) {
   try {
     connectToDatabase()
@@ -59,4 +63,25 @@ export async function createQuestion (params: CreateQuestionParams) {
 
     revalidatePath(path)
   } catch (error) {}
+}
+
+export async function getQuestionById (params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase()
+
+    const { questionId } = params
+
+    const question = await Question.findById(questionId)
+      .populate({ path: 'tags', model: Tag, select: '_id name' })
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id clerkId name picture'
+      })
+
+    return question
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
