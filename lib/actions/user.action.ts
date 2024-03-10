288fead25a14048c2e1bab715f8beac6a32a0688
Vidit Shah <vidit0210@protonmail.com>
2024-03-10 12:31:02 +0530

    Added Upvotes

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,8 +1,8 @@
-'use server'
+'use server';
 
-import { FilterQuery } from 'mongoose'
-import User from '@/database/user.model'
-import { connectToDatabase } from '../mongoose'
+import { FilterQuery } from 'mongoose';
+import User from '@/database/user.model';
+import { connectToDatabase } from '../mongoose';
 import {
   CreateUserParams,
   DeleteUserParams,
@@ -11,68 +11,68 @@ import {
   GetUserByIdParams,
   GetUserStatsParams,
   ToggleSaveQuestionParams,
-  UpdateUserParams
-} from './shared.types'
-import { revalidatePath } from 'next/cache'
-import Question from '@/database/question.model'
-import Tag from '@/database/tag.model'
-import Answer from '@/database/answer.model'
+  UpdateUserParams,
+} from './shared.types';
+import { revalidatePath } from 'next/cache';
+import Question from '@/database/question.model';
+import Tag from '@/database/tag.model';
+import Answer from '@/database/answer.model';
 
-export async function getUserById (params: any) {
+export async function getUserById(params: any) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { userId } = params
+    const { userId } = params;
 
-    const user = await User.findOne({ clerkId: userId })
+    const user = await User.findOne({ clerkId: userId });
 
-    return user
+    return user;
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function createUser (userData: CreateUserParams) {
+export async function createUser(userData: CreateUserParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const newUser = await User.create(userData)
+    const newUser = await User.create(userData);
 
-    return newUser
+    return newUser;
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function updateUser (params: UpdateUserParams) {
+export async function updateUser(params: UpdateUserParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { clerkId, updateData, path } = params
+    const { clerkId, updateData, path } = params;
 
     await User.findOneAndUpdate({ clerkId }, updateData, {
-      new: true
-    })
+      new: true,
+    });
 
-    revalidatePath(path)
+    revalidatePath(path);
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function deleteUser (params: DeleteUserParams) {
+export async function deleteUser(params: DeleteUserParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { clerkId } = params
+    const { clerkId } = params;
 
-    const user = await User.findOneAndDelete({ clerkId })
+    const user = await User.findOneAndDelete({ clerkId });
 
     if (!user) {
-      throw new Error('User not found')
+      throw new Error('User not found');
     }
 
     // Delete user from database
@@ -82,56 +82,56 @@ export async function deleteUser (params: DeleteUserParams) {
     // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
 
     // delete user questions
-    await Question.deleteMany({ author: user._id })
+    await Question.deleteMany({ author: user._id });
 
     // TODO: delete user answers, comments, etc.
 
-    const deletedUser = await User.findByIdAndDelete(user._id)
+    const deletedUser = await User.findByIdAndDelete(user._id);
 
-    return deletedUser
+    return deletedUser;
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function getAllUsers (params: GetAllUsersParams) {
+export async function getAllUsers(params: GetAllUsersParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { searchQuery } = params
+    const { searchQuery } = params;
 
-    const query: FilterQuery<typeof User> = {}
+    const query: FilterQuery<typeof User> = {};
 
     if (searchQuery) {
       query.$or = [
         { name: { $regex: new RegExp(searchQuery, 'i') } },
-        { username: { $regex: new RegExp(searchQuery, 'i') } }
-      ]
+        { username: { $regex: new RegExp(searchQuery, 'i') } },
+      ];
     }
 
-    const users = await User.find(query).sort({ createdAt: -1 })
+    const users = await User.find(query).sort({ createdAt: -1 });
 
-    return { users }
+    return { users };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function toggleSaveQuestion (params: ToggleSaveQuestionParams) {
+export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { userId, questionId, path } = params
+    const { userId, questionId, path } = params;
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
     if (!user) {
-      throw new Error('User not found')
+      throw new Error('User not found');
     }
 
-    const isQuestionSaved = user.saved.includes(questionId)
+    const isQuestionSaved = user.saved.includes(questionId);
 
     if (isQuestionSaved) {
       // remove question from saved
@@ -139,123 +139,123 @@ export async function toggleSaveQuestion (params: ToggleSaveQuestionParams) {
         userId,
         { $pull: { saved: questionId } },
         { new: true }
-      )
+      );
     } else {
       // add question to saved
       await User.findByIdAndUpdate(
         userId,
         { $addToSet: { saved: questionId } },
         { new: true }
-      )
+      );
     }
 
-    revalidatePath(path)
+    revalidatePath(path);
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function getSavedQuestions (params: GetSavedQuestionsParams) {
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { clerkId, searchQuery } = params
+    const { clerkId, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, 'i') } }
-      : {}
+      : {};
 
     const user = await User.findOne({ clerkId }).populate({
       path: 'saved',
       match: query,
       options: {
-        sort: { createdAt: -1 }
+        sort: { createdAt: -1 },
       },
       populate: [
         { path: 'tags', model: Tag, select: '_id name' },
-        { path: 'author', model: User, select: '_id clerkId name picture' }
-      ]
-    })
+        { path: 'author', model: User, select: '_id clerkId name picture' },
+      ],
+    });
 
     if (!user) {
-      throw new Error('User not found')
+      throw new Error('User not found');
     }
 
-    const savedQuestions = user.saved
+    const savedQuestions = user.saved;
 
-    return { questions: savedQuestions }
+    return { questions: savedQuestions };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function getUserInfo (params: GetUserByIdParams) {
+export async function getUserInfo(params: GetUserByIdParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
-    const { userId } = params
+    const { userId } = params;
 
-    const user = await User.findOne({ clerkId: userId })
+    const user = await User.findOne({ clerkId: userId });
 
     if (!user) {
-      throw new Error('User not found')
+      throw new Error('User not found');
     }
 
-    const totalQuestions = await Question.countDocuments({ author: user._id })
-    const totalAnswers = await Answer.countDocuments({ author: user._id })
+    const totalQuestions = await Question.countDocuments({ author: user._id });
+    const totalAnswers = await Answer.countDocuments({ author: user._id });
 
     return {
       user,
       totalQuestions,
-      totalAnswers
-    }
+      totalAnswers,
+    };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function getUserQuestions (params: GetUserStatsParams) {
+export async function getUserQuestions(params: GetUserStatsParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
     // const { userId, page = 1, pageSize = 10 } = params
-    const { userId } = params
+    const { userId } = params;
 
-    const totalQuestions = await Question.countDocuments({ author: userId })
+    const totalQuestions = await Question.countDocuments({ author: userId });
 
     const userQuestions = await Question.find({ author: userId })
       .sort({ views: -1, upvotes: -1 })
       .populate('tags', '_id name')
-      .populate('author', '_id clerkId name picture')
+      .populate('author', '_id clerkId name picture');
 
-    return { totalQuestions, questions: userQuestions }
+    return { totalQuestions, questions: userQuestions };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
-export async function getUserAnswers (params: GetUserStatsParams) {
+export async function getUserAnswers(params: GetUserStatsParams) {
   try {
-    connectToDatabase()
+    connectToDatabase();
 
     // const { userId, page = 1, pageSize = 10 } = params
-    const { userId } = params
+    const { userId } = params;
 
-    const totalAnswers = await Answer.countDocuments({ author: userId })
+    const totalAnswers = await Answer.countDocuments({ author: userId });
 
     const userAnswers = await Answer.find({ author: userId })
       .sort({ upvotes: -1 })
       .populate('question', '_id title')
-      .populate('author', '_id clerkId name picture')
+      .populate('author', '_id clerkId name picture');
 
-    return { totalAnswers, answers: userAnswers }
+    return { totalAnswers, answers: userAnswers };
   } catch (error) {
-    console.log(error)
-    throw error
+    console.log(error);
+    throw error;
   }
 }
 
