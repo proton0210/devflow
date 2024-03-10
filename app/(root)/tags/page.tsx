e8b7cadf8fce85935190630387a14b35d0ee3bc0
Vidit Shah <vidit0210@protonmail.com>
2024-03-10 12:49:00 +0530

    Added Skeleton and Tags Page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,12 +1,18 @@
 import Filter from '@/components/shared/Filter'
 import NoResult from '@/components/shared/NoResult'
+import Pagination from '@/components/shared/Pagination'
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar'
-import { UserFilters } from '@/constants/filters'
+import { TagFilters } from '@/constants/filters'
 import { getAllTags } from '@/lib/actions/tag.actions'
+import { SearchParamsProps } from '@/types'
 import Link from 'next/link'
 
-const Page = async () => {
-  const result = await getAllTags({})
+const Page = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllTags({
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1
+  })
 
   return (
     <>
@@ -22,7 +28,7 @@ const Page = async () => {
         />
 
         <Filter
-          filters={UserFilters}
+          filters={TagFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
         />
       </div>
@@ -62,6 +68,13 @@ const Page = async () => {
           />
             )}
       </section>
+
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div>
     </>
   )
 }
